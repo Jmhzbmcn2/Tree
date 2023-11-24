0d8304cfd0b5779a94a3bf9cb811a3a64172b879
VUDUYLINH <vuduylinh150804@gmail.com>
2023-11-24 16:31:10 +0700

    Test

---
+++ src/homework1/ex_1/Main.java
@@ -0,0 +1,11 @@
+package homework1.ex_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintInput testMain = new PrintInput();
+        testMain.print(sc);
+    }
+}
